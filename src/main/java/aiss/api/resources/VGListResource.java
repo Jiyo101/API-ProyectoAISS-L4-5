b5ee8960ead220681ffd996fa1a3e08e5d01804e
Paco Vilaplana <fravilde1@alum.us.es>
2021-05-15 20:06:21 +0200

    Arreglar paths que tenian /get/

--- src/main/java/aiss/api/resources/VGListResource.java
+++ src/main/java/aiss/api/resources/VGListResource.java
@@ -84,7 +84,7 @@ public class VGListResource {
 	
 	
 	@GET
-	@Path("/get/{vglistId}")
+	@Path("/{vglistId}")
 	@Produces("application/json")
 	public VGList get(@PathParam("vglistId") String id)
 	{
@@ -98,7 +98,7 @@ public class VGListResource {
 	}
 	
 	@GET
-	@Path("/get/{type}")
+	@Path("/{type}")
 	@Produces("application/json")
 	public Set<VGList> getType(@PathParam("type") Type type)
 	{
